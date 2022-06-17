46d38e88c13a20dcab00ebf8a5149333820ef84a
Elias Heinzen <elias.heinzen@sc.senai.br>
2022-06-17 08:12:42 -0300

    Abstraido DadosPessoais e criado nova classe Terceirizado

--- src/br/com/alura/rh/model/Funcionario.java
+++ src/br/com/alura/rh/model/Funcionario.java
@@ -1,72 +1,44 @@
 package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.time.LocalDate;
-
-import br.com.alura.rh.ValidacaoException;
 
 public class Funcionario {
 
-	private String nome;
-	private String cpf;
-	private Cargo cargo;
-	private BigDecimal salario;
-	private LocalDate dataUltimoReajuste;
+    private DadosPessoais dadosPessoais;
+    private LocalDate dataUltimoReajuste;
 
-	public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		this.nome = nome;
-		this.cpf = cpf;
-		this.cargo = cargo;
-		this.salario = salario;
-	}
+    public Funcionario(String nome, String cpf, Cargo cargo, BigDecimal salario) {
+        this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+    }
 
-	public void atualizarSalario(BigDecimal novoSalario) {
-		this.salario = novoSalario;
-		this.dataUltimoReajuste = LocalDate.now();
-	}
+    public void atualizarSalario(BigDecimal novoSalario) {
+        this.dadosPessoais.setSalario(novoSalario);
+        this.dataUltimoReajuste = LocalDate.now();
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public void promover(Cargo novoCargo) {
+        this.dadosPessoais.setCargo(novoCargo);
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return dadosPessoais.getNome();
+    }
 
-	public String getCpf() {
-		return cpf;
-	}
+    public String getCpf() {
+        return dadosPessoais.getCpf();
+    }
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    public Cargo getCargo() {
+        return dadosPessoais.getCargo();
+    }
 
-	public Cargo getCargo() {
-		return cargo;
-	}
+    public BigDecimal getSalario() {
+        return dadosPessoais.getSalario();
+    }
 
-	public void setCargo(Cargo cargo) {
-		this.cargo = cargo;
-	}
+    public LocalDate getDataUltimoReajuste() {
+        return dataUltimoReajuste;
+    }
 
-	public BigDecimal getSalario() {
-		return salario;
-	}
-
-	public void setSalario(BigDecimal salario) {
-		this.salario = salario;
-	}
-
-	public LocalDate getDataUltimoReajuste() {
-		return dataUltimoReajuste;
-	}
-
-	public void setDataUltimoReajuste(LocalDate dataUltimoReajuste) {
-		this.dataUltimoReajuste = dataUltimoReajuste;
-	}
-
-	public void promover(Cargo novoCargo) {
-		this.cargo = novoCargo;
-	}
 }
